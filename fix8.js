4fcae697a6825a547c42fd51ca1b6ed2f6e65aae
Mikailou <mikailou33@gmail.com>
2026-06-10 15:58:48 +0300

    Nettoyage fix8

--- fix8.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-var html=fs.readFileSync('malitaxi-vip-final-12.html','utf8');
-var start=html.indexOf("BILLETS D'AVION");
-var fnStart=html.lastIndexOf("if(svc===",start);
-var fnEnd=html.indexOf("if(svc===",fnStart+10);
-html=html.slice(0,fnStart)+html.slice(fnEnd);
-fs.writeFileSync('malitaxi-vip-final-12.html',html);
-console.log('OK fait');
