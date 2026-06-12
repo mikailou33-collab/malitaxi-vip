0f5233f1ba22cd0a832bcfd4b5d45dcb0b2ae6b8
Mikailou <mikailou33@gmail.com>
2026-06-12 15:03:09 +0300

    Nettoyage

--- fix-admin.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-var html=fs.readFileSync('malitaxi-admin-10.html','utf8');
-html=html.replace('${CHAUFFEURS.filter(c=>!c.valide).length}','3');
-html=html.replace('Billets Avion</td>','');
-html=html.replace(/545\.000 F<\/td>/,'');
-html=html.replace('✈️','');
-fs.writeFileSync('malitaxi-admin-10.html',html);
-console.log('OK fait');
