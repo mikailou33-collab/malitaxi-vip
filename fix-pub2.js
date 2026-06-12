bb35ee3e5c8a951806fc483391826b243790c473
Mikailou <mikailou33@gmail.com>
2026-06-12 15:37:38 +0300

    Nettoyage

--- fix-pub2.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-var c=fs.readFileSync('malitaxi-admin-10.html','utf8');
-var pubDiv=c.substring(c.indexOf('<div id="section-pub"'),c.indexOf('</div>',c.indexOf('</table>',c.indexOf('section-pub')))+6);
-c=c.replace(pubDiv,'');
-var mainEnd=c.indexOf('</div>',c.indexOf('<div class="main">'));
-c=c.slice(0,mainEnd)+pubDiv+c.slice(mainEnd);
-fs.writeFileSync('malitaxi-admin-10.html',c);
-console.log('OK fait');
