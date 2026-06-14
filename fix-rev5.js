f640d5d52a870916f45b0782881f63ae324fb6a9
Mikailou <mikailou33@gmail.com>
2026-06-14 13:42:01 +0300

    supprime fichiers fix temporaires

--- fix-rev5.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let h = fs.readFileSync('chauffeur-vip-pro-20.html', 'utf8');
-const s = h.indexOf('\nfunction openRevenus()');
-const e = h.indexOf('\n}', s) + 2;
-if(s > 0){ h = h.slice(0, s) + h.slice(e); console.log('Supprimé'); }
-fs.writeFileSync('chauffeur-vip-pro-20.html', h, 'utf8');
-console.log('OK taille:', h.length);
